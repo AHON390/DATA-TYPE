e70f8d26aaf87ede10cbd9f1ae125cb3df5d910b
AHON390 <dark201100000@gmail.com>
2025-04-26 17:09:52 +0600

    Create information.py

---
+++ information.py
@@ -0,0 +1,9 @@
+# string datatype a set of characters
+your_name =input("enter your name : ")
+#int datatype
+your_birthday_date=input("enter your birthday : ")
+
+place_of_birth=input("enter your place : ")  
+
+
+print(f" {your_name} your bithday date is {your_birthday_date} and your place of birth is {place_of_birth}.")
