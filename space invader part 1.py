ea3523b99fe96dfa502a06b6a268b5580996d718
AHON390 <dark201100000@gmail.com>
2025-08-22 15:43:26 +0600

    Create space invader part 1.py

---
+++ space invader part 1.py
@@ -0,0 +1,60 @@
+import math
+import random
+import pygame
+screen_width = 800
+screen_height = 600
+player_start_x=370
+player_start_y=380
+enemy_start_y_min=50
+enemy_start_y_max=150
+enemy_speed_x = 4
+enemy_speed_y=40
+bullet_speed_y=10
+collision_distance = 27
+pygame.init()
+screen=pygame.display.set_mode((screen_width,screen_height))
+background=pygame.image.load('background.png')
+pygame.display.set_caption("space invader")
+icon=pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+#player
+playerImg=pygame.image.load("player.png")
+playerX=player_start_x
+playerY=player_start_y
+playerX_change=0
+enemyImg=[]
+enemyX=[]
+enemyY=[]
+enemyX_change=[]
+enemyY_change=[]
+num_of_enemies=6
+for _i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0,screen_width - 64))
+    enemyY.append(random.randint(enemy_start_y_min,enemy_start_y_max)
+    enemyX_change.append(enemy_speed_x)
+    enemyY_change.append(enemy_speed_y)
+bulletImg=pygame.image.load('bullet.png')
+bulletX=0
+bulletY=player_start_y
+bulletY_change=bullet_speed_y
+bullet_state='ready'
+score_value=0
+font=pygame.font.Font("freesansbold.ttf",32)
+textX=10
+textY=10
+over_font=pygame.font.Font('freesansbold.ttf',64)
+def show_score(x,y)
+    score=font.render("score:"+str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
+def game_over_text():
+    over_text=over_font.render("game over",True,(255,255,255))
+    screen.blit(over_text,(200,250))
+def player(x,y)
+    screen.blit(playerImg,(x,y))
+def enemy(x,y,i):
+    screen.b;it(enemyImg[i],(x,y))
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state='fire'
+    screen.blit(bulletImg,(x+16,y+10))
